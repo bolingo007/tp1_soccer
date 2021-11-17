9fe9aeb5626a16361ebdbebdef1e9e66ea59938e
Benoit <b.robichaud12@gmail.com>
2021-11-17 14:13:27 -0500

    Asie

--- Asie.php
+++ Asie.php
@@ -8,8 +8,7 @@
   <?php include "menu.html";?>
 
 <div>
-  zone Asie à faire
-</div>
+  La fédération asiatique de football (l'AFC) déservant le plus gros continent au monde autant en population qu'en superficie bénéficie que d'une maigre 4.5(4 places en qualifications et une place inter-confédération ) places de qualifications pour la Coupe du Monde qatari de 2022. Le Qatar étant le pays hôte cependant, il bénificie d'une qualification automatique pour la ronde finale. Le tournoi de qualification est structuré sous 4 rondes. Une ronde préliminaire incluant les équipes classée de 35e à 46e pour ressortir 6 pays en vue de la deuxième ronde. Celle-ci inclue les 40 meilleurs équipes du continent, soit 8 groupes de 5 équipes qui s'affrontent en tournoi a la ronde. À l'issue de la deuxième ronde, les 8 gagnants des groupes et les 4 meilleurs deuxièmes se qualifieront pour le troisième tour. Ces douzes équipes s'affronteront sur 2 groupes et les 2 meilleurs équipes de chaque groupes se qualifient. Les 2 troisièmes sont finalement envoyé dans un tournoi inter-confédération pour la dernière place disponible. </div>
 
 </body>
 </html>
