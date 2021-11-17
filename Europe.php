c8816022451f40c14afed2c5ba2baaa7d7432c1d
Benoit <b.robichaud12@gmail.com>
2021-11-17 13:57:23 -0500

    Europe

--- Europe.php
+++ Europe.php
@@ -8,7 +8,7 @@
   <?php include "menu.html";?>
 
 <div>
-  à faire
+  L'Angleterre étant considéré comme le berceau du ballon-rond organisé lors de la création en 1863 de la FA (Football Association), il est évident qu'il est impossible de traiter de la Coupe du Monde sans parler des superpuissance de foot Européenne. Le vieux continent est en effet le continent avec le plus de pays victorieux, incluant 12 championnats (4 en Allemagne et Italie, 2 en France, 1 en Angleterre et en Espagne) il en va de soit que sortir de la qualification dans cette région est d'autant plus laborieuse. 13 pays obtiendrons leurs laisser-passer pour la phase finale au Qatar en décembre 2022 et ce sur les 55 pays membre de l'UEFA (Union of European Football Associations). Les qualifications comporte ainsi un tournoi à la ronde incluant 5 groupes de 5 équipes et 5 groupes de 6 équipes. Les gagnants des 10 groupes sont automatiquement qualifié pour la Coupe du Monde et les 10 deuxièmes de chaque groupe ainsi que les 2 meilleures équipes de la Coupe des Nations, rentrent dans un mini-tournoi à simple élimination pour qualifié les 3 dernières équipes. 
 </div>
 
 </body>
