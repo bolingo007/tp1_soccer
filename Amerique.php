6c4ed918bb4e94de13b8c3065d08c30ca43a2ff2
Benoit <b.robichaud12@gmail.com>
2021-11-17 14:27:46 -0500

    Amerique

--- Amerique.php
+++ Amerique.php
@@ -8,7 +8,7 @@
   <?php include "menu.html";?>
 
 <div>
-  Je vais le faire
+  Les qualifications de la Coupe du Monde de la FIFA au Qatar en 2022 offre 4.5 places pour les équipes d'Amérique du Sud (CONMEBOL) et 3.5 pour la fédération Nord-Américaine de soccer (CONCACAF). Deux tournoi de qualifications séparés sont ainsi disputé en vue de la Coupe du Monde. D'un côté, le CONMEBOL (CONfederacion SudaMEricana de FutBOL) délimite ses représentants lors d'un simple tournoi à la ronde de 10 équipes ou les quatres meilleurs équipes se qualifient automatiquement et la cinquième avancera au tournoi inter-confédération pour la dernière place disponible. Du côté de l'hémisphère nord, la CONCACAF (Confédération de football d'Amérique du Nord, d'Amérique centrale et des Caraîbes) présente un tournoi en 3 étapes. Premièrement, les 6 meilleurs équipes s'affrontent dans "l'Hexagone" et les 3 meilleurs équipes se qualifient automatiquement, la quatrième affrontera le gagnant d'un tournoi impliquant les 29 autres équipes sous 8 groupes pour s'assurer une place dans le tournoi inter-condérération.  
 </div>
 
 </body>
