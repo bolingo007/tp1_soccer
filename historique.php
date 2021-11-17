807927e9f6ac47aa94be39de2ababc10d863a96f
Benoit <b.robichaud12@gmail.com>
2021-11-17 13:43:13 -0500

    Historique

--- historique.php
+++ historique.php
@@ -8,7 +8,7 @@
   <?php include "menu.html";?>
 
 <div>
-  ajouter l'historique des coupes du monde
+    L'institution internationale qu'est la Coupe du Monde de Soccer (ou Football) débutat son premier tournoi en Uruguay en 1930. Sous l'égide de son premier président Jules Rimet, la FIFA(Fédération International de Football Amateur) organisa la première Coupe du Monde qui fut contesté par un total de 13 éqipes de trois continents. Remportée par le pays hôte, cette première édition démontra dès lors l'énorme popularité de cette compétition qui saura perdurée pour 90 ans et 21 éditions du tournoi aux 4 ans. En effet, dès la deuxième édition de 1934, le tournoi fut grandement expandu, invitant 16 équipes en phase finale jusqu'à 32 à partir de 1998 et 46 pour l'édition à venir aux États-Unis en 2026. Attirant plus de 35 milliards de téléspectateurs cumulés à travers les années, la coupe du monde de soccer est l'un des événements sportifs les plus populaire de l'histoire de l'humanité. L'impact socio-économique d'une telle compétition pousse les États hôtes à payer des sommes faramineuses en installations sportives et logistiques. Les retombées touristiques et le rayonnement à l'international pousse, en effet, plusieurs pays sont ainsi prèt à payer des sommes faramineurs afin de porter leurs candidatures pour hébergé le plus gros tournoi de soccer au même titre que les prestigieux Jeux Olympiques. 
 </div>
 
 </body>
