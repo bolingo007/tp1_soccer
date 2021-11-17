08a81ad2a37c028a7535d7ffffc2e0bdb5dab7a3
Benoit <b.robichaud12@gmail.com>
2021-11-17 14:38:36 -0500

    Amerique v0.2

--- Amerique.php
+++ Amerique.php
@@ -8,7 +8,12 @@
   <?php include "menu.html";?>
 
 <div>
-  Les qualifications de la Coupe du Monde de la FIFA au Qatar en 2022 offre 4.5 places pour les équipes d'Amérique du Sud (CONMEBOL) et 3.5 pour la fédération Nord-Américaine de soccer (CONCACAF). Deux tournoi de qualifications séparés sont ainsi disputé en vue de la Coupe du Monde. D'un côté, le CONMEBOL (CONfederacion SudaMEricana de FutBOL) délimite ses représentants lors d'un simple tournoi à la ronde de 10 équipes ou les quatres meilleurs équipes se qualifient automatiquement et la cinquième avancera au tournoi inter-confédération pour la dernière place disponible. Du côté de l'hémisphère nord, la CONCACAF (Confédération de football d'Amérique du Nord, d'Amérique centrale et des Caraîbes) présente un tournoi en 3 étapes. Premièrement, les 6 meilleurs équipes s'affrontent dans "l'Hexagone" et les 3 meilleurs équipes se qualifient automatiquement, la quatrième affrontera le gagnant d'un tournoi impliquant les 29 autres équipes sous 8 groupes pour s'assurer une place dans le tournoi inter-condérération.  
+Malgré la domination Européenne de l’ère moderne, les Amériques (plus particulièrement l’Amérique du Sud) sont reconnues comme des superpuissance de soccer. En effet, le Brésil est le pays le plus titré de l’histoire de la Coupe du Monde avec 5 championnats (1958, 1962, 1970, 1994 et 2002) et l’Argentine, avec ces 2 victoires (1978 et 1986). 
+
+Les qualifications de la Coupe du Monde de la FIFA au Qatar en 2022 offre 4.5 places pour les équipes d'Amérique du Sud (CONMEBOL) et 3.5 pour la fédération Nord-Américaine de soccer (CONCACAF). Deux tournois de qualifications séparés sont ainsi disputé en vue de la Coupe du Monde. D'un côté, le CONMEBOL (CONfederacion SudaMEricana de FutBOL) délimite ses représentants lors d'un simple tournoi à la ronde de 10 équipes ou les quatre meilleures équipes se qualifient automatiquement et la cinquième avancera au tournoi inter-confédération pour la dernière place disponible. 
+
+Du côté de l'hémisphère nord, la CONCACAF (Confédération de football d'Amérique du Nord, d'Amérique centrale et des Caraïbes) présente un tournoi en 3 étapes. Premièrement, les 6 meilleurs équipes s'affrontent dans "l'Hexagone" et les 3 meilleurs équipes se qualifient automatiquement, la quatrième affrontera le gagnant d'un tournoi impliquant les 29 autres équipes sous 8 groupes pour s'assurer une place dans le tournoi inter-confédération.  
+
 </div>
 
 </body>
