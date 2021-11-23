67a2168f740d9cf4a1c50c5350a62e0b74b8c528
Benoit <b.robichaud12@gmail.com>
2021-11-23 17:15:46 -0500

    STRONGER

--- Amerique.php
+++ Amerique.php
@@ -14,8 +14,8 @@
     <br/>
     <br/>
     <h2>Les fédérations américaines</h2>
-    <p style="font-size: 1.2rem;"> Malgré la domination <a href="./Europe.php"> Européenne </a> de l’ère moderne, <strong>les Amériques</strong> (plus particulièrement <a href="https://en.wikipedia.org/wiki/CONMEBOL"> l’Amérique du Sud </a>) sont reconnues comme des superpuissances de <a href="https://en.wikipedia.org/wiki/Association_football"> soccer </a>. En effet, le <a href="https://en.wikipedia.org/wiki/Brazilian_Football_Confederation"> Brésil </a> est le pays le plus titré de l’histoire de la <a href="https://en.wikipedia.org/wiki/FIFA_World_Cup"> <strong> Coupe du Monde </strong> </a> avec 5 championnats (1958, 1962, 1970, 1994 et 2002) et <a href="https://en.wikipedia.org/wiki/Argentine_Football_Association"> l’Argentine </a>, avec ces 2 victoires (1978 et 1986). 
-      Les qualifications de la <a href="https://en.wikipedia.org/wiki/FIFA_World_Cup"> <strong> Coupe du Monde </strong> de la FIFA </a> au <a href="https://www.fifa.com/tournaments/mens/worldcup/qatar2022"> Qatar en 2022 </a> offre 4.5 places pour les équipes d'Amérique du Sud (<a href="https://en.wikipedia.org/wiki/CONMEBOL"><strong>CONMEBOL</strong></a>) et 3.5 pour la fédération Nord-Américaine de soccer (<a href="https://en.wikipedia.org/wiki/CONCACAF"><strong> CONCACAF </strong></a>). Deux tournois de qualifications
+    <p style="font-size: 1.2rem;"> Malgré la domination <a href="./Europe.php"> Européenne </a> de l’ère moderne, <strong>les Amériques</strong> (plus particulièrement <a href="https://en.wikipedia.org/wiki/CONMEBOL"> l’Amérique du Sud </a>) sont reconnues comme des superpuissances de <a href="https://en.wikipedia.org/wiki/Association_football"> soccer </a>. En effet, le <a href="https://en.wikipedia.org/wiki/Brazilian_Football_Confederation"> Brésil </a> est le pays le plus titré de l’histoire de la <a href="https://en.wikipedia.org/wiki/FIFA_World_Cup"> <strong> Coupe du Monde </strong> </a> avec 5 <strong>championnats</strong> (1958, 1962, 1970, 1994 et 2002) et <a href="https://en.wikipedia.org/wiki/Argentine_Football_Association"> l’Argentine </a>, avec ces 2 victoires (1978 et 1986). 
+      Les qualifications de la <a href="https://en.wikipedia.org/wiki/FIFA_World_Cup"> <strong> Coupe du Monde </strong> de la FIFA </a> au <a href="https://www.fifa.com/tournaments/mens/worldcup/qatar2022"> Qatar en 2022 </a> offre 4.5 places pour les équipes d'Amérique du Sud (<a href="https://en.wikipedia.org/wiki/CONMEBOL"><strong>CONMEBOL</strong></a>) et 3.5 pour la fédération Nord-Américaine de soccer (<a href="https://en.wikipedia.org/wiki/CONCACAF"><strong> CONCACAF </strong></a>). Deux <strong>tournois de qualifications </strong>
       séparés sont ainsi disputé en vue de la <a href="https://en.wikipedia.org/wiki/FIFA_World_Cup"> <strong> Coupe du Monde </strong> </a>. </p>
     <br/>
     <br/>
@@ -24,7 +24,7 @@
       <a href="https://en.wikipedia.org/wiki/CONMEBOL">
         <img class="pull-left m-3 pb-3" src="./images/CONMEBOL.png" alt="CONMEBOL"></a>
     </div>
-    <p style="font-size: 1.2rem;">  D'un côté, le <strong>CONMEBOL</strong> (CONfederacion SudaMEricana de FutBOL) délimite ses représentants lors d'un simple tournoi à la ronde de 10 équipes ou les quatre meilleures équipes se qualifient automatiquement et la 
+    <p style="font-size: 1.2rem;">  D'un côté, le <strong>CONMEBOL</strong> (CONfederacion SudaMEricana de FutBOL) délimite ses représentants lors d'un simple <strong>tournoi à la ronde</strong> de 10 équipes ou les quatre meilleures équipes se qualifient automatiquement et la 
       cinquième avancera au tournoi <a href="https://en.wikipedia.org/wiki/FIFA_World_Cup_qualification#Intercontinental_play-offs"> inter-confédération </a> pour la dernière place disponible. </p>
     <br/>
     <br/>
@@ -35,8 +35,8 @@
         <img class="pull-left m-3 pb-3" src="./images/CONCACAF.png" alt="CONCACAF"></a>
     </div>
     <p style="font-size: 1.2rem;">Du côté de l'hémisphère nord, la <strong> CONCACAF </strong> (Confédération de football d'Amérique du Nord,
-     d'Amérique centrale et des Caraïbes) présente un tournoi en 3 étapes. Premièrement, les 6 meilleurs équipes s'affrontent dans <a href="https://en.wikipedia.org/wiki/Hexagonal_(CONCACAF)#:~:text=In%20association%20football%2C%20the%20term,six%20remaining%20teams%20in%20CONCACAF"> "l'Hexagone" </a> et les 3 meilleurs équipes se qualifient automatiquement, la quatrième affrontera le gagnant d'un tournoi
-     impliquant les 29 autres équipes sous 8 groupes pour s'assurer une place dans le tournoi inter-confédération.  </p>
+     d'Amérique centrale et des Caraïbes) présente un <strong>tournoi</strong> en 3 étapes. Premièrement, les 6 meilleurs <strong>équipes</strong> s'affrontent dans <a href="https://en.wikipedia.org/wiki/Hexagonal_(CONCACAF)#:~:text=In%20association%20football%2C%20the%20term,six%20remaining%20teams%20in%20CONCACAF"> "l'Hexagone" </a> et les 3 meilleurs équipes se qualifient automatiquement, la quatrième affrontera le gagnant d'un tournoi
+     impliquant les 29 autres équipes sous <strong>8 groupes</strong> pour s'assurer une place dans le <strong>tournoi inter-confédération</strong>.  </p>
 
 </div>
 <?php include "footer.html";?>
